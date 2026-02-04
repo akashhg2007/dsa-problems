e1ec852f5c06d5343ab30d3c6fa2e7a578e4bdf7
akash-hg <akashhga@gmail.com>
2026-02-04 16:13:41 +0530

    git commit

---
+++ src/methods/Sum.java
@@ -0,0 +1,17 @@
+package methods;
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        sum();
+    }
+         static void sum(){
+        Scanner in = new Scanner(System.in);
+             System.out.print("enter the first number:");
+             int num1=in.nextInt();
+             System.out.print("enter the second number:");
+             int num2=in.nextInt();
+             int sum=num1+num2;
+             System.out.println("The sum is: "+sum);
+    }
+}
