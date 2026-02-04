dabe217928850a9471b0be7fa21531e4b90f3ad1
akash-hg <akashhga@gmail.com>
2026-02-04 14:14:04 +0530

    git commit

--- src/switchcase/Switch.java
+++ src/switchcase/Switch.java
@@ -6,6 +6,20 @@ public class Switch {
         Scanner in= new Scanner(System.in);
         System.out.print("enter the fruits name here :");
         String fruit = in.next();
+ /*       switch(fruit){
+             case "apple":
+                 System.out.println("a red sweet fruit");
+                 break;
+             case "mango":
+                 System.out.println("a king of fruits");
+                 break;
+             case "banana":
+                 System.out.println("a small fruit");
+                 break;
+             default:
+                 System.out.println("invalid fruit");
+        }
+  */
         switch (fruit) {
             case "apple" -> System.out.println("a red sweet fruit");
             case "mango" -> System.out.println("a king of fruits");
