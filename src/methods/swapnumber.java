ccfc3d91c3c113a95272ecd71d7d63099adb163d
akash-hg <akashhga@gmail.com>
2026-02-04 18:02:47 +0530

    git commit

---
+++ src/methods/swapnumber.java
@@ -0,0 +1,23 @@
+package methods;
+import java.util.Scanner;
+
+public class swapnumber {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the first number:");
+        int a = in.nextInt();
+        System.out.println("enter the second number:");
+        int b = in.nextInt();
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a=" +a );
+        System.out.println("b=" +b);
+
+      //  static void swap ( int a, int b){
+        //    int temp = a;
+         //   a = b;
+//b = temp;
+        }
+    }
+
