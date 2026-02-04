705aedf9aaedc35a18551aaa018754b1f8f7f4b7
akash-hg <akashhga@gmail.com>
2026-02-04 17:14:17 +0530

    git commit

---
+++ src/methods/Stringexample.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class Stringexample {
+    public static void main(String[] args) {
+       String massage = greet();
+        System.out.println(massage);
+    }
+    static String greet() {
+        String greeting = "hello Akash";
+        return greeting;
+    }
+}
---
+++ src/methods/Sum_other_ex.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class Sum_other_ex {
+    public static void main(String[] args) {
+        int ans = sum(10,50);
+        System.out.println("The sum is: "+ans);
+    }
+    static int sum(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+}
