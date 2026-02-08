6a2c2319c54da12830f381845a01cf2097274818
Akash h g <akashhga@gmail.com>
2026-02-09 01:17:37 +0530

    git commit

---
+++ Arrays/src/Arrayss/Main.java
@@ -0,0 +1,27 @@
+package Arrayss;
+
+public class Main {
+    static void main(String[] args) {
+        //Q: store a roll number in an array
+        int a = 10;
+        //Q: store a person's name
+        String name = "akash";
+
+        //Q store 5 roll number
+        int rno1 = 23;
+        int rno2 = 73;
+        int rno3 = 73;
+        int rno4 = 28;
+        int rno5 = 43;
+
+        //for array
+        //syntax is
+        //datatype[] variable_name = new datatype[size];
+        // store 5 roll number
+
+        int[] rnos = new int[5];
+        // or direct
+        int[] rnos2 = {23, 54, 78, 90, 34};
+
+    }
+}
