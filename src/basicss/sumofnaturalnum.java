4921ba69868cecbe62c036d6986436069301d21e
Akash h g <akashhga@gmail.com>
2026-02-28 13:05:03 +0530

    git commit

---
+++ src/basicss/sumofnaturalnum.java
@@ -0,0 +1,13 @@
+package basicss;
+
+public class sumofnaturalnum {
+    static void main(String[] args) {
+        int count = 0;
+        while (count < 5) {
+            int a=count+1;
+            System.out.println(a);
+            count ++;
+
+        }
+    }
+}
