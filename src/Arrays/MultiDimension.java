bdacda3f9336ed2fc0ae97ad93600653acc25be0
Akash h g <akashhga@gmail.com>
2026-04-14 22:50:33 +0530

    git commit

--- src/Arrays/MultiDimension.java
+++ src/Arrays/MultiDimension.java
@@ -5,21 +5,31 @@ import java.util.Scanner;
 import java.util.Scanner;
 
 public class MultiDimension {
-    Scanner in = new Scanner(System.in);
-
-
-
-
     public static void main(String[] args) {
-        int[][] arr ={
-                {1,4,6},
-                {4,7,3},
-                {7,8,9}
-        };
-        for (int i = 0; i <3; i++) {
+        Scanner in = new Scanner(System.in);
+        //int[][] arr ={
+         //       {1,4,6},//0th index
+         //       {4,3},//1st index
+         //       {7,8,9} //2nd index -> arr[2]={7,8,9}
+        //};
+       // for (int i = 0; i <3; i++) {
+        int[][] arr=new int[3][2];
+        //System.out.println(arr.length);// no of rows
+
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col <arr[row].length ; col++) {
+                arr[row][col] = in.nextInt();
+
+                System.out.print(arr[row][col] + " ");
+
+            }
+
+            }
 
         }
-        System.out.println(Arrays.toString(arr));
-    }
 
-}
+        }
+      // System.out.println(Arrays.toString(arr));
+    
+
+
