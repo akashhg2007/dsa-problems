3ce909892365ca0a897f49f081a503e65dc297f0
Akash h g <akashhga@gmail.com>
2026-08-28 22:15:44 +0530

    COMMIT

--- src/Arrays/printPairs.java
+++ src/Arrays/printPairs.java
@@ -2,16 +2,16 @@ package Arrays;
 
 public class printPairs {
     public static void Printpairs(int num[]){
+        int tp=0;
         for (int i = 0; i < num.length ; i++) {
             int curr = num[i];
             for (int j = i+1; j <num.length ; j++) {
                 System.out.print("(" +curr+ "," +num[j]+ ")");
-
-
+                tp++;
             }
             System.out.println();
-
         }
+        System.out.println("total pairs =" +tp);
     }
 
     static void main(String[] args) {
